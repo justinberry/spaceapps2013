38a241285bb6004c844006eaa613890dd3a71855
Zhi Qiao <zhi.qiao@gmail.com>
2013-04-20 20:43:38 +1000

    changes to location lib

--- annual_solar.py
+++ annual_solar.py
@@ -3,19 +3,37 @@ from google.appengine.ext import db
 # Local imports
 import location
 
+
 class AnnualSolarPotential(db.Model):
-    latitude = db.FloatProperty(required=True)
-    longitude = db.FloatProperty(required=True)
+    lat = db.FloatProperty(required=True)
+    lng = db.FloatProperty(required=True)
     last_updated = db.DateTimeProperty(auto_now=True, auto_now_add=True)
+    country = db.StringProperty()
+    region = db.StringProperty()
+    city = db.StringProperty()
+    potential = db.FloatProperty()
+    actual = db.FloatProperty()
 
 
 class AnnualSolar(object):
     """Provides data about annual solar capacity at given lat/lang."""
-    
-    def __init__(self, latitude, longitude):
-        self.latitude = latitude
-        self.longitude = longitude
+
+    def __init__(self, lat, lng):
+        self.lat = lat
+        self.lng = lng
 
     def GetClosestPotential(self):
         """Returns the AnnualSolarPotential from the closest known location."""
+        country, region, city = location.GetLocationParts(
+            self.lat, self.lng)
+        where_clauses = ["country = %s" % country]
+        if region:
+            where_clauses.append("region = %s" % region)
+        if city:
+            where_clauses.append("city = %s" % city)
+        query = db.GqlQuery(
+            "SELECT lat, lng FROM AnnualSolarPotential WHERE %s"
+            % " AND ".join(where_clauses))
+        candidates = query.run(limit=100)  # TODO(zhi)
+        destinations = [(x.lat, x.lng) for x in candidates]
         
--- location.py
+++ location.py
@@ -1,20 +1,76 @@
+import sys
 from django.utils import simplejson
 
 from google.appengine.api import urlfetch
 
 
-MAPS_API_TEMPLATE = (
+MAPS_API_GEOCODE_TMPL = (
     "http://maps.googleapis.com/maps/api/geocode/json?"
     "latlng=%(latlng)s&sensor=false")
 
+MAPS_API_GEOCODE_NAME_TMPL = (
+    "http://maps.googleapis.com/maps/api/geocode/json?"
+    "address=%(address)s&sensor=false")
 
-def GetLocationName(latitude, longitude):
-    by_country, by_region, by_city = GetLocationParts(latitude, longitude)
+MAPS_API_DISTANCE_TMPL = (
+    "http://maps.googleapis.com/maps/api/distancematrix/json?"
+    "origins=%(origins)s&destinations=%(destinations)s&sensor=false")
+
+
+def GetDistances(lat, lng, candidates):
+    """Distances from a given lat/long to a list of candidate (lat/long).
+
+    Args:
+      lat: Latitude, float.
+      lng: Longitude, float.
+      candidates:  List of objects with a .lat and .lng attribute.
+    
+    Returns:
+      A list of distances (int) indexed 
+    """
+    url = MAPS_API_DISTANCE_TMPL % {
+        "origins": "%s,%s" % (lat, lng),
+        "destinations": "|".join(
+            ["%s,%s" % (c.lat, c.lng) for c in candidates])
+        }
+    result = urlfetch.fetch(url)
+    distance_response = simplejson.loads(result.content)
+    distances = [
+        d["distance"]["value"] for d in distance_response["rows"][0]["elements"]]
+    return distances
+
+
+def GetMinCandidate(lat, lng, candidates):
+    """From a list of candidates, return the one closest to the given lat/lng.
+
+    Args:
+      lat: Latitude, float.
+      lng: Longitude, float.
+      candidates:  List of objects with a .lat and .lng attribute.
+    
+    Returns:
+      A (lat, lng) of the location closest to the given lat/lng.
+    """
+    distances = GetDistances(lat, lng, candidates)
+    min_distance = sys.maxint
+    min_idx = -1
+    for i, d in enumerate(distances):
+        print i, d
+        if d < min_distance:
+            min_distance = d
+            min_idx = i
+    print min_idx
+    print candidates
+    return candidates[min_idx].lat, candidates[min_idx].lng
+
+
+def GetLocationName(lat, lng):
+    by_country, by_region, by_city = GetLocationParts(lat, lng)
     return by_city or by_region or by_country or "Unknown"
 
 
-def GetLocationParts(latitude, longitude):
-    url = MAPS_API_TEMPLATE % {"latlng": "%s,%s" % (latitude, longitude)}
+def GetLocationParts(lat, lng):
+    url = MAPS_API_GEOCODE_TMPL % {"latlng": "%s,%s" % (lat, lng)}
     result = urlfetch.fetch(url)
     location_response = simplejson.loads(result.content)
     by_country = None
@@ -28,3 +84,13 @@ def GetLocationParts(latitude, longitude):
         elif "country" in res["types"]:
             by_country = res["formatted_address"]
     return (by_country, by_region, by_city)
+
+
+def GetLatitudeLongitude(common_name):
+    "Return lat/long for a given common name."
+    url = MAPS_API_GEOCODE_NAME_TMPL % {
+        "address": common_name.replace(" ", "+")}
+    result = urlfetch.fetch(url)
+    location_response = simplejson.loads(result.content)
+    location = location_response["results"][0]["geometry"]["location"]
+    return location["lat"], location["lng"]
