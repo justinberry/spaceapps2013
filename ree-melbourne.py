d4ce029c72007b38dc26b043e2b05d9c69b557a0
Zhi Qiao <zhi.qiao@gmail.com>
2013-04-20 16:26:23 +1000

    refactor

--- ree-melbourne.py
+++ ree-melbourne.py
@@ -4,32 +4,15 @@ import webapp2
 
 from django.utils import simplejson
 
-from google.appengine.api import urlfetch
 from google.appengine.api import users
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
 
-MAPS_API_TEMPLATE = (
-    "http://maps.googleapis.com/maps/api/geocode/json?"
-    "latlng=%(latlng)s&sensor=false")
+# Local imports
+import location
+
 
 class Home(webapp2.RequestHandler):
-
-    def GetLocation(self, latitude, longitude):
-        url = MAPS_API_TEMPLATE % {"latlng": "%s,%s" % (latitude, longitude)}
-        result = urlfetch.fetch(url)
-        location_response = simplejson.loads(result.content)
-        by_postal_code = None
-        by_locality = None
-        by_country = None
-        for res in location_response["results"]:
-            if "postal_code" in res["types"]:
-                by_postal_code = res["formatted_address"] 
-            elif "locality" in res["types"]:
-                by_locality = res["formatted_address"] 
-            elif "country" in res["types"]:
-                by_country = res["formatted_address"] 
-        return by_postal_code or by_locality or by_country or "Unknown"
 
     def get(self):
         latitude = self.request.get("lat", default_value=-37.814107)
@@ -41,8 +24,8 @@ class Home(webapp2.RequestHandler):
             % (latitude, longitude, user_time))
 
         self.response.out.write(
-            "We think you are in %s<br/>" % self.GetLocation(
-                latitude, longitude))
+            "We think you are in %s<br/>" 
+            % location.GetLocationName(latitude, longitude))
 
 
 
